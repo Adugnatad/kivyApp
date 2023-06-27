243a888fc9f8d8fa2efbe68a70b9a2f1f03b310a
Adugna Tadesse <adugtad@gmail.com>
2023-06-27 15:57:48 +0300

    latest

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ class PongGame(Widget):
     def on_touch_move(self, touch):
         if touch.x < self.width / 3:
             self.player1.center_y = touch.y
+            
         if touch.x > self.width - self.width / 3:
             self.player2.center_y = touch.y
 
