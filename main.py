ccda870931b8472682c57848c4d8003d13b81087
Adugna Tadesse <adugtad@gmail.com>
2023-06-27 15:58:06 +0300

    latest

--- main.py
+++ main.py
@@ -48,13 +48,14 @@ class PongGame(Widget):
         if self.ball.x < self.x:
             self.player2.score += 1
             self.serve_ball(vel=(4, 0))
+            
         if self.ball.right > self.width:
             self.player1.score += 1
             self.serve_ball(vel=(-4, 0))
     def on_touch_move(self, touch):
         if touch.x < self.width / 3:
             self.player1.center_y = touch.y
-            
+
         if touch.x > self.width - self.width / 3:
             self.player2.center_y = touch.y
 
